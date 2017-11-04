542ece573b16db1f74a3537aa9c0361d08ef453a
Parthiban Rajendran <parthi2929@gmail.com>
2017-11-04 20:20:53 +0530

    listening via http server

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
+//1. Import
 var express = require("express");
-var routes = require('./routes/route.js');
+var routes = require('./routes/route.js');  
+var http = require("http");
 
+//2. Initialize
 var app = express();
-
+var server = http.Server(app);
 app.set("view engine", "ejs");
-
 app.use(express.static(__dirname + "/public"));
 
+
+//3. Routing
 app.get("/", routes.home);
 app.get("/:user", routes.userRoute);
 
-var port = process.env.PORT || 8080;
 
-var server = app.listen(
+//4. Listening
+var port = process.env.PORT || 8080;
+server.listen(
     port,
     function(request, response)
     {
